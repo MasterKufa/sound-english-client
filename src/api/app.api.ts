913e0a5b3f9f4c897d779c0aca7dd030549158ab
Roman Kaluger <r.kaluger@sftpro.ru>
2023-07-28 22:34:38 +0300

    env

--- src/api/app.api.ts
+++ src/api/app.api.ts
@@ -3,7 +3,7 @@ import { socket } from "@master_kufa/client-tools";
 
 export const connectSocketFx = createEffect(() =>
   socket.connect(
-    `${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}`,
-    `http://${process.env.REACT_APP_AUTH_HOST}/auth`
+    `${process.env.REACT_APP_SERVER_HOST}`,
+    `${process.env.REACT_APP_AUTH_HOST}/auth`
   )
 );
