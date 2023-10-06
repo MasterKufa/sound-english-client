909e99d83a0d34b29f41d7eee4b6282d15ea4de8
Roman Kaluger <r.kaluger@sftpro.ru>
2023-10-06 22:53:50 +0300

    go to word from reminder

--- src/modules/reminder/reminder.module.tsx
+++ src/modules/reminder/reminder.module.tsx
@@ -1,8 +1,11 @@
 import { useStoreMap } from "effector-react";
 import { vocabularyModel } from "../../models";
-import { Box, Chip } from "@mui/material";
+import { Box, Chip, IconButton } from "@mui/material";
 import { vocabularySelectors } from "../../models/vocabulary";
 import { WordContainer } from "./reminder.styles";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import { navigation } from "../../shared/navigate";
+import { Paths } from "../../app/app.types";
 
 type ReminderProps = {
   id: number;
@@ -18,6 +21,12 @@ export const Reminder = ({ id }: ReminderProps) => {
     <Box sx={WordContainer}>
       <Chip label={reminder?.sourceWord.text} />
       <Chip label={reminder?.targetWord.text} variant="outlined" />
+      <IconButton
+        size="small"
+        onClick={() => `${navigation.navigate(Paths.vocabulary)}/${id}`}
+      >
+        <OpenInNewIcon />
+      </IconButton>
     </Box>
   );
 };
--- src/modules/reminder/reminder.styles.ts
+++ src/modules/reminder/reminder.styles.ts
@@ -2,7 +2,7 @@ import { SxProps } from "@mui/material";
 
 export const WordContainer: SxProps = {
   display: "grid",
-  gridTemplateColumns: "1fr 1fr",
+  gridTemplateColumns: "1fr 1fr 36px",
   flexGrow: 1,
   gap: 1,
   paddingTop: 1,
