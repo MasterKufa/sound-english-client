158675ce558a4432a716fd6a2735416f1c99eb45
Roman Kaluger <r.kaluger@sftpro.ru>
2023-12-23 22:31:27 +0300

    new deploy

--- src/app/app.types.ts
+++ src/app/app.types.ts
@@ -1,10 +1,10 @@
 export enum Paths {
-  vocabulary = "/sound-english-client/vocabulary",
-  settings = "/sound-english-client/settings",
-  player = "/sound-english-client/player",
+  vocabulary = "/vocabulary",
+  settings = "/settings",
+  player = "/player",
 
   // path that does not match tabs must be below
-  word = "/sound-english-client/vocabulary/:id",
-  root = "/sound-english-client",
-  fileUpload = "/sound-english-client/vocabulary/file-upload",
+  word = "/vocabulary/:id",
+  root = "/",
+  fileUpload = "/vocabulary/file-upload",
 }
